6b46e488a9e3f4bdeeb350604dd00581ef29d025
Austin Ziegler <austin@zieglers.ca>
2019-06-09 19:46:24 -0400

    v1.0: Initial version

---
+++ README.md
@@ -0,0 +1,35 @@
+# halostatue/fish-go
+
+A quick plugin for <a href="https://fishshell.com" title="friendly
+interactive shell">fish shell</a> to ensure that GOROOT and GOPATH are always
+set and that $GOROOT/bin and $GOPATH/bin are set properly in $PATH (via
+fish_user_paths).
+
+[![GitHub tag](https://img.shields.io/github/tag/halostatue/fish-go.svg)](https://github.com/halostatue/fish-go/releases)
+
+## Installation
+
+Install with [Fisher](https://github.com/jorgebucaran/fisher) (recommended):
+
+```fish
+fisher add halostatue/fish-go
+```
+
+<details>
+<summary>Not using a package manager?</summary>
+
+---
+
+Copy `conf.d/*.fish` to your fish configuration directory preserving the
+directory structure.
+</details>
+
+### System Requirements
+
+- [fish](https://github.com/fish-shell/fish-shell) 3.0+
+
+## Functions
+
+## License
+
+[MIT](LICENCE.md)
---
+++ conf.d/halostatue_fish_go.fish
@@ -0,0 +1,45 @@
+if not command -sq go
+    # If there is no go executable, stop with a warning.
+    echo 1>2 'Binary for go is not installed.'
+    exit
+end
+
+# GOROOT should be set when this is done, either from a pre-existing
+# environment variable, a Fish universal variable, or by setting a universal
+# variable from go env GOROOT.
+set -gq GOROOT
+or set -Uq GOROOT
+or set -Ux GOROOT (go env GOROOT)
+
+# GOPATH should be set when this is done, either from a pre-existing
+# environment variable, a Fish universal variable, or by setting a universal
+# variable from go env GOPATH, or from $HOME/go if the former is empty.
+set -gq GOPATH
+or set -Uq GOPATH
+or begin
+    set -Ux GOPATH (go env GOPATH)
+    test -z GOPATH
+    and set -Ux GOPATH $HOME/go
+
+    mkdir -p $GOPATH
+end
+
+# Ensure that $GOROOT/bin and $GOPATH/bin are added to fish_user_paths
+contains -- $GOROOT/bin $fish_user_paths
+or set fish_user_paths $fish_user_paths $GOROOT/bin
+contains -- $GOPATH/bin $fish_user_paths
+or set fish_user_paths $GOPATH/bin $fish_user_paths
+
+function _halostatue_fish_go_uninstall -e halostatue_fish_go_uninstall
+    set -l i (contains -i -- $GOROOT/bin $fish_user_paths)
+    and set -e fish_user_paths[$i]
+
+    set -l i (contains -i -- $GOPATH/bin $fish_user_paths)
+    and set -e fish_user_paths[$i]
+
+    set -Uq GOPATH
+    and set -e GOPATH
+
+    set -Uq GOROOT
+    and set -e GOROOT
+end
