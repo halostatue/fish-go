7f67d4d5aac1e155b734e83fe107397f8e5eb640
Austin Ziegler <austin@zieglers.ca>
2020-06-15 00:25:43 -0400

    Clean up the installer more

--- conf.d/halostatue_fish_go.fish
+++ conf.d/halostatue_fish_go.fish
@@ -4,38 +4,41 @@ if not command -sq go
     exit
 end
 
-# GOROOT should be set when this is done, either from a pre-existing
-# environment variable, a Fish universal variable, or by setting a universal
-# variable from go env GOROOT.
-set -gq GOROOT
-or set -Uq GOROOT
-or set -Ux GOROOT (go env GOROOT)
+function _halostatue_fish_go_update_path -a old_path new_path
+    set -l i (contains -i $old_path/bin $fish_user_paths)
+    and set -e fish_user_paths[$i]
 
-# GOPATH should be set when this is done, either from a pre-existing
-# environment variable, a Fish universal variable, or by setting a universal
-# variable from go env GOPATH, or from $HOME/go if the former is empty.
-set -gq GOPATH
-or set -Uq GOPATH
-or begin
-    set -Ux GOPATH (go env GOPATH)
-    test -z GOPATH
-    and set -Ux GOPATH $HOME/go
-
-    mkdir -p $GOPATH
+    if test -n "$new_path"
+        contains -- $new_path/bin $fish_user_paths
+        or set fish_user_paths $fish_user_paths $new_path/bin
+    end
 end
 
-# Ensure that $GOROOT/bin and $GOPATH/bin are added to fish_user_paths
-contains -- $GOROOT/bin $fish_user_paths
-or set fish_user_paths $fish_user_paths $GOROOT/bin
-contains -- $GOPATH/bin $fish_user_paths
-or set fish_user_paths $GOPATH/bin $fish_user_paths
+function _halostatue_fish_go_update_variable -a varname default
+    set -l new_value (go env $varname)
+    set -l old_value $$varname
+
+    test -n "$default" -a -z "$new_value"
+    and set new_value $default
+
+    if test "$old_value" != "$new_value"
+        _halostatue_fish_go_update_path $old_value $new_value
+
+        if set -gq $varname
+            set -gx $varname $new_value
+        else
+            set -Ux $varname $new_value
+        end
+    end
+end
+
+_halostatue_fish_go_update_variable GOROOT
+_halostatue_fish_go_update_variable GOPATH $HOME/go
+and mkdir -p $GOPATH
 
 function _halostatue_fish_go_uninstall -e halostatue_fish_go_uninstall
-    set -l i (contains -i -- $GOROOT/bin $fish_user_paths)
-    and set -e fish_user_paths[$i]
-
-    set -l i (contains -i -- $GOPATH/bin $fish_user_paths)
-    and set -e fish_user_paths[$i]
+    _halostatue_fish_go_update_path $GOROOT
+    _halostatue_fish_go_update_path $GOPATH
 
     set -Uq GOPATH
     and set -e GOPATH
@@ -43,5 +46,5 @@ function _halostatue_fish_go_uninstall -e halostatue_fish_go_uninstall
     set -Uq GOROOT
     and set -e GOROOT
 
-    functions -e (status function)
+    functions -e (functions -a | command awk '/_halostatue_fish_go_/')
 end
